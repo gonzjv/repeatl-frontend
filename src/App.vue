655bfeb05f4bee63f4b695b249dced65fd022b6b
gonzjv <gonzjv@ya.ru>
2022-10-05 00:16:28 +0300

    build: tailwind.css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <h1 class="text-green-400">TAILWIND TEST!!!</h1>
+
   <HelloWorld msg="Vite + Vue" />
 </template>
 
