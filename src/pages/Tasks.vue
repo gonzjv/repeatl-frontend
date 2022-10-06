9cb5c3ca155bf3fd347a16ce7220aa7553b10a5f
gonzjv <gonzjv@ya.ru>
2022-10-07 00:03:28 +0300

    feat: task list

--- src/pages/Tasks.vue
+++ src/pages/Tasks.vue
@@ -2,8 +2,19 @@
 import { onBeforeMount, ref } from 'vue';
 
 let tasksData = ref();
-onBeforeMount(async () => {});
+onBeforeMount(async () => {
+  const response = await fetch(
+    `http://localhost:4000/courses`
+  );
+  tasksData.value = await response.json();
+  console.log('taskData', tasksData.value);
+});
 </script>
 <template>
   <h2 class="text-yellow-400">Tasks is there!</h2>
+  <ul>
+    <li v-for="task in tasksData">
+      {{ task.name }}
+    </li>
+  </ul>
 </template>
