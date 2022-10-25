2307d7036efa265b70be6821548c2cbe066edda3
gonzjv <gonzjv@ya.ru>
2022-10-25 15:44:34 +0300

    feat: add login form

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -3,17 +3,22 @@ import { storeToRefs } from 'pinia';
 import { useDisplayStore } from '../store/display';
 
 const store = useDisplayStore();
-const { isSectionPopupDisplay } =
-  storeToRefs(store);
+const {
+  isPopupDisplay,
+  isLoginPopupDisplay,
+  isSectionPopupDisplay,
+} = storeToRefs(store);
 </script>
 <template>
   <aside
     @click="
       store.$patch({
+        isPopupDisplay: false,
+        isLoginPopupDisplay: false,
         isSectionPopupDisplay: false,
       })
     "
-    v-if="isSectionPopupDisplay"
+    v-if="isPopupDisplay"
     class="z-10 w-full h-full absolute left-0 top-0"
   ></aside>
 </template>
--- src/components/PopupLogin.vue
+++ src/components/PopupLogin.vue
@@ -2,6 +2,11 @@
 import { storeToRefs } from 'pinia';
 import { useCourseStore } from '../store/course';
 import { useDisplayStore } from '../store/display';
+import {
+  reactive,
+  toRefs,
+  onBeforeMount,
+} from 'vue';
 
 const displayStore = useDisplayStore();
 const courseStore = useCourseStore();
@@ -9,11 +14,49 @@ const { isLoginPopupDisplay } =
   storeToRefs(displayStore);
 const { currentSection, currentCollection } =
   storeToRefs(courseStore);
+
+const state = reactive({
+  isLoginMode: true,
+});
+
+const { isLoginMode } = toRefs(state);
 </script>
 <template>
   <div
-    class="py-5 flex flex-col gap-5 z-20 blur-0 bg-white shadow-lg absolute top-[25vh] left-[20vw] w-6/12 h-96 overflow-auto"
+    class="py-5 flex flex-col gap-5 z-20 blur-0 bg-white shadow-lg absolute top-[12vh] left-[40vw] w-4/12 h-96 overflow-auto rounded-md"
   >
-    <h3>Login Popup</h3>
+    <form class="w-full p-5" action="">
+      <input
+        class="w-full"
+        type="text"
+        placeholder="Эл. почта"
+      />
+      <input
+        class="w-full"
+        type="text"
+        placeholder="Пароль"
+      />
+      <input
+        v-if="!isLoginMode"
+        class="w-full"
+        type="text"
+        placeholder="Пароль еще разок"
+      />
+      <button>
+        {{
+          isLoginMode
+            ? 'ВОЙТИ'
+            : 'ЗАРЕГИСТРИРОВАТЬСЯ'
+        }}
+      </button>
+    </form>
+    <aside class="flex gap-5">
+      <p class="text-xs font-extralight">
+        Не зарегистрированы?
+      </p>
+      <button @click="isLoginMode = true">
+        Регистрация
+      </button>
+    </aside>
   </div>
 </template>
