d7fba1c7f9db400e75c67a0540693c855b02e661
gonzjv <gonzjv@ya.ru>
2022-10-26 09:20:14 +0300

    feat: user sign up

--- src/components/PopupLogin.vue
+++ src/components/PopupLogin.vue
@@ -3,6 +3,7 @@ import { storeToRefs } from 'pinia';
 import { useCourseStore } from '../store/course';
 import { useDisplayStore } from '../store/display';
 import { reactive, toRefs } from 'vue';
+import { addUser } from '@/services/userService';
 
 const displayStore = useDisplayStore();
 const courseStore = useCourseStore();
@@ -15,15 +16,34 @@ const state = reactive({
   isLoginMode: true,
   email: '',
   password: '',
+  newUser: {},
 });
-const { isLoginMode, email, password } =
+const { isLoginMode, email, password, newUser } =
   toRefs(state);
+
+const handleSubmit = async () => {
+  if (isLoginMode.value) {
+  }
+  if (!isLoginMode.value) {
+    const user = {
+      email: email.value,
+      password: password.value,
+      role: 'user',
+    };
+
+    const createdUser = await addUser(user);
+    if (createdUser) {
+      newUser.value = createdUser;
+    }
+  }
+};
 </script>
 <template>
   <div
     class="py-5 flex flex-col justify-center gap-5 z-20 blur-0 bg-white shadow-lg absolute top-[12vh] left-[40vw] w-4/12 h-96 overflow-auto rounded-md"
   >
     <form
+      @submit.prevent="handleSubmit"
       class="w-full p-5 flex flex-col gap-7"
       action=""
     >
@@ -46,6 +66,7 @@ const { isLoginMode, email, password } =
         placeholder="Пароль еще разок"
       />
       <button
+        type="submit"
         class="bg-gradient-to-r from-cyan-400 via-yellow-200 to-pink-400 rounded-full h-10 text-slate-400 font-bold"
       >
         {{
@@ -56,29 +77,38 @@ const { isLoginMode, email, password } =
       </button>
     </form>
     <aside
-      class="flex gap-5 justify-center items-center"
+      class="flex flex-col gap-5 justify-center items-center px-5"
     >
-      <p class="text-xs font-extralight">
-        {{
-          isLoginMode
-            ? 'Не зарегистрированы?'
-            : 'Уже зарегистрированы?'
-        }}
+      <p
+        class="text-emerald-400 font-light"
+        v-if="newUser.email"
+      >
+        Добро пожаловать, {{ newUser.email }}.
+        Можете смело нажать
       </p>
-      <button
-        class="text-sky-400"
-        v-if="isLoginMode"
-        @click="isLoginMode = false"
-      >
-        Регистрация
-      </button>
-      <button
-        class="text-sky-400"
-        v-if="!isLoginMode"
-        @click="isLoginMode = true"
-      >
-        Вход
-      </button>
+      <div class="flex gap-5 items-center">
+        <p class="text-xs font-extralight">
+          {{
+            isLoginMode
+              ? 'Не зарегистрированы?'
+              : 'Уже зарегистрированы?'
+          }}
+        </p>
+        <button
+          class="text-sky-400"
+          v-if="isLoginMode"
+          @click="isLoginMode = false"
+        >
+          Регистрация
+        </button>
+        <button
+          class="text-sky-400"
+          v-if="!isLoginMode"
+          @click="isLoginMode = true"
+        >
+          Вход
+        </button>
+      </div>
     </aside>
   </div>
 </template>
---
+++ src/services/userService.js
@@ -0,0 +1,34 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+const getUser = async (userId) => {
+  try {
+    const response = await fetch(
+      `${API_URL}users/${userId}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Erorr:', error);
+  }
+};
+
+const addUser = async (user) => {
+  const options = {};
+  const url = `${API_URL}users/`;
+  const reqData = user;
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(reqData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Erorr:', error);
+  }
+};
+
+export { getUser, addUser };
