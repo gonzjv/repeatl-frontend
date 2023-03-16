457197c96814265a1351b836b37b4bbae2f4b3ee
gonzjv <gonzjv@ya.ru>
2023-03-16 13:24:04 +0300

    feat: model sections to repeat at the same day

---
+++ src/components/TodayTaskList.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+import {
+  reactive,
+  toRefs,
+  onBeforeMount,
+} from 'vue';
+import { useUserStore } from '../store/user';
+import { getCompletedModelSectionArr } from '@/services/modelService';
+import { useCourseStore } from '../store/course';
+
+const userStore = useUserStore();
+const { userData } = storeToRefs(userStore);
+
+const courseStore = useCourseStore();
+
+const state = reactive({
+  completedSectionArr: [],
+});
+
+const { completedSectionArr } = toRefs(state);
+onBeforeMount(async () => {
+  completedSectionArr.value =
+    await getCompletedModelSectionArr(
+      userData.value.token,
+      userData.value.id
+    );
+  console.log(
+    'completedSectionArr',
+    completedSectionArr.value
+  );
+});
+</script>
+<template>
+  <ul class="shadow-lg rounded-lg p-5">
+    <p class="text-emerald-400">User data</p>
+    <p>{{ userData }}</p>
+    <p class="text-emerald-400">
+      Completed section arr
+    </p>
+    <p>{{ completedSectionArr }}</p>
+
+    <li
+      class="flex gap-3"
+      v-for="section in completedSectionArr"
+    >
+      <div class="flex gap-3">
+        <p>
+          id
+          {{ section.id }}
+        </p>
+        <p>
+          number
+          {{ section.number }}
+        </p>
+      </div>
+      <router-link
+        @click="
+          courseStore.$patch({
+            currentSection: section,
+          })
+        "
+        to="/questRepeat"
+        class="text-sky-400 font-bold w-1/5 border-sky-400 border-2 rounded-md"
+      >
+        Повторение
+      </router-link>
+    </li>
+  </ul>
+</template>
--- src/pages/TodayTask.vue
+++ src/pages/TodayTask.vue
@@ -1,37 +1,9 @@
 <script setup>
-import {
-  onBeforeMount,
-  toRefs,
-  reactive,
-} from 'vue';
-import { useUserStore } from '../store/user';
-import { getCompletedModelSectionArr } from '@/services/modelService';
-import { storeToRefs } from 'pinia';
-
-const userStore = useUserStore();
-const { userData } = storeToRefs(userStore);
-
-const state = reactive({
-  completedSectionArr: [],
-});
-
-const { completedSectionArr } = toRefs(state);
-onBeforeMount(async () => {
-  completedSectionArr.value =
-    await getCompletedModelSectionArr(
-      userData.value.token,
-      userData.value.id
-    );
-  console.log(
-    'completedSectionArr',
-    completedSectionArr.value
-  );
-});
+import TodayTaskList from '../components/TodayTaskList.vue';
 </script>
 <template>
-  <main>
-    <h2>Today task list</h2>
-    <p>{{ userData }}</p>
-    <p>{{ completedSectionArr }}</p>
+  <main class="flex flex-col gap-5">
+    <aside>Filter models/words</aside>
+    <TodayTaskList />
   </main>
 </template>
